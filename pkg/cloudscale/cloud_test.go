8d2a68a3476f865b7cafefaadc8b3a6471aa4ccf
Denis Krienbühl <denis.krienbuehl@cloudscale.ch>
2023-06-26 12:46:54 +0200

    Split provider tests

--- pkg/cloudscale/cloud_test.go
+++ pkg/cloudscale/cloud_test.go
@@ -23,34 +23,44 @@ func TestMaskAccessToken(t *testing.T) {
 	}
 }
 
-func TestNewCloudscaleProvider(t *testing.T) {
+func TestNewCloudscaleProviderWithoutToken(t *testing.T) {
 	if _, err := newCloudscaleProvider(nil); err == nil {
 		t.Error("no token in env: newCloudscaleProvider should have failed")
 	}
+}
 
+func TestNewCloudscaleProviderWithToken(t *testing.T) {
 	os.Setenv(AccessToken, "1234")
 	if _, err := newCloudscaleProvider(nil); err != nil {
 		t.Error("newCloudscaleProvider should initialize with just a token")
 	}
+}
 
+func TestNewCloudscaleProviderWithBadConfig(t *testing.T) {
 	cfg := iotest.ErrReader(errors.New("bad config"))
 	if _, err := newCloudscaleProvider(cfg); err != nil {
 		t.Error("newCloudscaleProvider should ignore the config file")
 	}
+}
 
+func TestNewCloudscaleProviderWithDefaultTimeout(t *testing.T) {
 	provider, _ := newCloudscaleProvider(nil)
 	if cs, _ := provider.(*cloud); cs.timeout != (5 * time.Second) {
 		t.Errorf("unexpected default timeout: %s", cs.timeout)
 	}
+}
 
+func TestNewCloudscaleProviderWithInvalidTimeout(t *testing.T) {
 	os.Setenv(ApiTimeout, "asdf")
-	provider, _ = newCloudscaleProvider(nil)
+	provider, _ := newCloudscaleProvider(nil)
 	if cs, _ := provider.(*cloud); cs.timeout != (5 * time.Second) {
 		t.Errorf("unexpected fallback timeout: %s", cs.timeout)
 	}
+}
 
+func TestNewCloudscaleProviderWithCustomTimeout(t *testing.T) {
 	os.Setenv(ApiTimeout, "10")
-	provider, _ = newCloudscaleProvider(nil)
+	provider, _ := newCloudscaleProvider(nil)
 	if cs, _ := provider.(*cloud); cs.timeout != (10 * time.Second) {
 		t.Errorf("ignored %s: %s", ApiTimeout, cs.timeout)
 	}
